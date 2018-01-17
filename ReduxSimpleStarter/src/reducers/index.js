3bbfe8f5cce72ed81452dd5e1b908d25d71d2f48
Jordan Schmidt <jordan@schmidt6.com>
2018-01-16 18:03:31 -0800

    added post creation, started post display

--- ReduxSimpleStarter/src/reducers/index.js
+++ ReduxSimpleStarter/src/reducers/index.js
@@ -1,8 +1,12 @@
-import { combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer} from 'redux-form';
 import PostsReducer from './reducer_posts.js';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
+
+//const store = createStore(rootReducer)
 
 export default rootReducer;
